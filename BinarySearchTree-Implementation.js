89001ef6e363b55680aec95a5be657cee7797d5f
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-06-08 10:41:50 -0500

    Binary tree lookup my solution

--- BinarySearchTree-Implementation.js
+++ BinarySearchTree-Implementation.js
@@ -17,41 +17,63 @@ class BinarySearchTree {
     if (!this.root) {
       this.root = newNode;
       return this;
-    } else {
-      let currentNode = this.root;
-      while (currentNode) {
-        if (currentNode.value > newNode.value) {
-          // go left
-          if (currentNode.left === null) {
-            currentNode.left = newNode;
-            return this;
-          }
-          currentNode = currentNode.left;
-        } else {
-          //go right
-          if (currentNode.right === null) {
-            currentNode.right = newNode;
-            return this;
-          }
-          currentNode = currentNode.right;
+    }
+    let currentNode = this.root;
+    while (currentNode) {
+      if (currentNode.value > newNode.value) {
+        // go left
+        if (currentNode.left === null) {
+          currentNode.left = newNode;
+          return this;
         }
+        currentNode = currentNode.left;
+      } else {
+        //go right
+        if (currentNode.right === null) {
+          currentNode.right = newNode;
+          return this;
+        }
+        currentNode = currentNode.right;
       }
     }
   }
-  lookup(value) {}
+  lookup(value) {
+    if (!this.root) {
+      return null;
+    }
+    let currentNode = this.root;
+    while (currentNode) {
+      if (currentNode.value===value) return currentNode;
+      if (currentNode.value > value) {
+        // go left
+        currentNode = currentNode.left;
+      } else {
+        //go right
+        currentNode = currentNode.right;
+      }
+    }
+    return null
+  }
 }
 const tree = new BinarySearchTree();
 tree.insert(9);
-tree.insert(20);
 tree.insert(4);
-tree.insert(1);
 tree.insert(6);
+tree.insert(20);
+tree.insert(170);
 tree.insert(15);
+tree.insert(1);
+console.log(traverse(tree.root));
 
-console.log(
-  util.inspect(tree.insert(170), {
-    showHidden: false,
-    depth: null,
-    colors: true,
-  })
-);
+console.log(tree.lookup(4))
+
+//     9
+//  4     20
+//1  6  15  170
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
