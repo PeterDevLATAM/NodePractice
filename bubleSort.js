0fb48d7d08b1a23c9cacea1a7bd13d2654c3fe1d
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-06-10 16:38:51 -0500

    my implementation of selection sort

--- bubleSort.js
+++ bubleSort.js
@@ -1,6 +1,6 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-function bubbleSort(array) {
+function bubbleSort(array) { //O(n^2) cuadratic solution
   let temp;
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1 - i; j++) {
---
+++ selectionSort.js
@@ -0,0 +1,24 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let sm = {
+      value: arr[i],
+      position: i,
+    };
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < sm.value) {
+        sm.value = arr[j];
+        sm.position = j;
+      }
+    }
+    //swap
+    let temp;
+    temp = arr[i];
+    arr[i] = sm.value;
+    arr[sm.position] = temp;
+  }
+  return arr;
+}
+
+console.log(selectionSort(numbers))
