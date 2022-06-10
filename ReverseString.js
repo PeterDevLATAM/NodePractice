4830ea5c75ad1f2843440c77d56cd6129e4ccfce
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-06-10 15:10:52 -0500

    reverse string recursive

--- ReverseString.js
+++ ReverseString.js
@@ -1,16 +1,21 @@
-
 //Reverse a String
 
 let str = "Hello my name is Peter";
 
 const reverseString = (string) => {
   let tempStr = "";
-  for (let i = string.length-1; i >= 0; i--) {
+  for (let i = string.length - 1; i >= 0; i--) {
     tempStr = tempStr + string[i];
   }
   return tempStr;
 };
 
-const reverseString2=(string)=> string.split('').reverse().join('')
-const reverseString3=(string)=> [...string].reverse().join('')
-console.log(reverseString2(str));
+const reverseString2 = (string) => string.split("").reverse().join("");
+const reverseString3 = (string) => [...string].reverse().join("");
+
+const reverseStringRecursive = (str) => {
+  if (str === "") return "";
+  return reverseStringRecursive(str.substr(1)) + str.charAt(0);
+};
+
+console.log(reverseStringRecursive(str));
